e67208d207254a6d28564ec3e2c97665764a3447
xhuang396 <93297520+xhuang396@users.noreply.github.com>
2022-10-22 17:58:04 -0500

    Update SecondHandController.java

--- src/main/java/com/tencent/wxcloudrun/controller/SecondHandController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/SecondHandController.java
@@ -3,6 +3,7 @@ package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.model.ProductType;
 import com.tencent.wxcloudrun.model.Response;
+import com.tencent.wxcloudrun.model.Product;
 import com.tencent.wxcloudrun.service.SecondHandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +21,15 @@ public class SecondHandController {
     @RequestMapping(value= {"/getProductList"}, method = {RequestMethod.GET})
     public Response getProductList(@RequestParam ProductType productType, @RequestParam Integer offset, @RequestParam Integer limit, HttpServletRequest request){
         return secondHandService.getProductList(productType,offset,limit);
+    }
+    
+    @RequestMapping(value= {"/collect"}, method = {RequestMethod.GET})
+    public Response collect(@RequestParam Product product, HttpServletRequest request){
+        return secondHandService.collect(product);
+    }
+    
+    @RequestMapping(value= {"/collect"}, method = {RequestMethod.GET})
+    public Response cancelCollect(@RequestParam Product product, HttpServletRequest request){
+        return secondHandService.collect(product);
     }
 }
