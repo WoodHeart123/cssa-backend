1bbf4d78539671e161079c8ff509f8d948af4346
PG <99935268+jpan82@users.noreply.github.com>
2022-11-05 00:06:29 -0500

    Update Product.java

--- src/main/java/com/tencent/wxcloudrun/model/Product.java
+++ src/main/java/com/tencent/wxcloudrun/model/Product.java
@@ -21,5 +21,82 @@ public class Product {
     private List<String> images;
     private String imagesJSON;
     private String delivery;
+    
+    // get no need to set
+    public Integer getproductID() {
+        return productID;
+    }
 
+    public Integer getUserID() {
+        return userID;
+    }
+    
+    public String getSellerAvatar() {
+        return sellerAvatar;
+    } 
+    
+    public Integer getSellerNickname() {
+        return sellerNickname;
+    }
+   
+    public Timestamp time() {
+        return time;
+    }
+
+    // need to set
+    public void getProductDescription() {
+        return productDescription;
+    }
+    
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+    
+    public void getPrice() {
+        return productDescription;
+    }
+    
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    
+    public void getProductType() {
+        return productType;
+    }
+    
+    public ProductType setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+    
+    public void getCondition() {
+        return condtion;
+    }
+    
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    
+    public void getProductTitle() {
+        return productTitle;
+    }
+    
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+    
+    public void getContact() {
+        return contact;
+    }
+    
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    
+    public void getDelivery() {
+        return delivery;
+    }
+    
+    public void setDelivery(String delivery) {
+        this.delivery = delivery;
+    }
 }
