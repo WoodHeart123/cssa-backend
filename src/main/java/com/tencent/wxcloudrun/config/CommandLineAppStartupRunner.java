091e411650c1e54179327b6233f30f34accda9e7
Boyan Sun <bsun68@wisc.edu>
2022-11-15 17:10:57 -0600

    fix get product list mapper

--- src/main/java/com/tencent/wxcloudrun/config/CommandLineAppStartupRunner.java
+++ src/main/java/com/tencent/wxcloudrun/config/CommandLineAppStartupRunner.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.clients.jedis.search.IndexDefinition;
 import redis.clients.jedis.search.IndexOptions;
 import redis.clients.jedis.search.RediSearchUtil;
@@ -32,35 +33,38 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     @Override
     public void run(String...args) {
-        try {
-            Schema sc = new Schema().addNumericField("courseID").addTextField("courseName", 1.0);
-            IndexDefinition def = new IndexDefinition().setPrefixes("course:");
-            jedisPooled.ftCreate("course-index", IndexOptions.defaultOptions().setDefinition(def), sc);
-        }catch(Exception ignored){}
-        ArrayList<Course> courseArrayList = (ArrayList<Course>) courseMapper.getAllCourseList(0, 10000, "courseNum", "ASC");
-        for (Course course : courseArrayList) {
-            Map<String, Object> fields = new HashMap<>();
-            fields.put("courseID", course.getCourseID());
-            if(course.getDepartmentAbrev().equals("COMP SCI")){
-                fields.put("courseName", "CS CS" + course.getCourseNum().toString());
-            }else {
-                fields.put("courseName",course.getDepartmentAbrev().replace(" ", "") + " " + course.getDepartmentAbrev().replace(" ", "")  + course.getCourseNum().toString());
+        try{
+            try {
+                Schema sc = new Schema().addNumericField("courseID").addTextField("courseName", 1.0);
+                IndexDefinition def = new IndexDefinition().setPrefixes("course:");
+                jedisPooled.ftCreate("course-index", IndexOptions.defaultOptions().setDefinition(def), sc);
+            }catch(Exception ignored){}
+            ArrayList<Course> courseArrayList = (ArrayList<Course>) courseMapper.getAllCourseList(0, 10000, "courseNum", "ASC");
+            for (Course course : courseArrayList) {
+                Map<String, Object> fields = new HashMap<>();
+                fields.put("courseID", course.getCourseID());
+                if(course.getDepartmentAbrev().equals("COMP SCI")){
+                    fields.put("courseName", "CS CS" + course.getCourseNum().toString());
+                }else {
+                    fields.put("courseName",course.getDepartmentAbrev().replace(" ", "") + " " + course.getDepartmentAbrev().replace(" ", "")  + course.getCourseNum().toString());
+                }
+                jedisPooled.hset("course:" + course.getCourseID().toString(), RediSearchUtil.toStringMap(fields));
             }
-            jedisPooled.hset("course:" + course.getCourseID().toString(), RediSearchUtil.toStringMap(fields));
-        }
 
-        try {
-            Schema sc = new Schema().addNumericField("productID").addTextField("productName", 1.0);
-            IndexDefinition def = new IndexDefinition().setPrefixes("product:");
-            jedisPooled.ftCreate("product-index", IndexOptions.defaultOptions().setDefinition(def), sc);
-        }catch(Exception ignored){}
-        ArrayList<Product> productArrayList = secondHandMapper.getAllProductList(0, 5000);
-        for (Product product : productArrayList) {
-            Map<String, Object> fields = new HashMap<>();
-            fields.put("productID", product.getProductID());
-            fields.put("productName", product.getProductTitle());
-            jedisPooled.hset("product:" + product.getProductID().toString(), RediSearchUtil.toStringMap(fields));
-        }
+            try {
+                Schema sc = new Schema().addNumericField("productID").addTextField("productName", 1.0);
+                IndexDefinition def = new IndexDefinition().setPrefixes("product:");
+                jedisPooled.ftCreate("product-index", IndexOptions.defaultOptions().setDefinition(def), sc);
+            }catch(Exception ignored){}
+            ArrayList<Product> productArrayList = secondHandMapper.getAllProductList(0, 5000);
+            for (Product product : productArrayList) {
+                Map<String, Object> fields = new HashMap<>();
+                fields.put("productID", product.getProductID());
+                fields.put("productName", product.getProductTitle());
+                jedisPooled.hset("product:" + product.getProductID().toString(), RediSearchUtil.toStringMap(fields));
+            }
+        }catch (JedisConnectionException ignored){}
+
 
 
 
