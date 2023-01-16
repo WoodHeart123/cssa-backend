9a54dd5e09780e8f035c1e41e0cb3522a473e1c7
xhuang396 <93297520+xhuang396@users.noreply.github.com>
2023-01-15 23:16:29 -0600

    Update RentalServiceImpl.java

--- src/main/java/com/tencent/wxcloudrun/service/impl/RentalServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/RentalServiceImpl.java
@@ -6,4 +6,28 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RentalServiceImpl implements RentalService {
+     @Override
+    public Response saveRentalInfo(Integer rentalID, boolean save, String userID) {
+        User user = RentalMapper.collect(userID);
+        List<Integer> rentalArrayList = JSON.parseArray(user.getSavedRentalJSON(), Integer.class);
+        if(save){
+          int i = 0;
+          if(!(rentalArrayList.contains(rentalID))){
+              i = 1;
+              rentalArrayList.add(rentalID);
+              String json = JSON.toJSONString(rentalArrayList);
+              user.setSavedRentalJSON(json);
+              RentalMapper.updateCollect(user);
+              if (i == 1) {
+                  return Response.builder().data(null).status(101).build();
+              } else {
+                  return Response.builder().data(null).status(100).build();
+              }
+          }
+        }
+        if(rentalArrayList.contains(rentalID)){
+           rentalArrayList.remove(rentalID);
+        }
+        
+    }
 }
