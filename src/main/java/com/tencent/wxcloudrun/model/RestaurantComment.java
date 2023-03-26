3e30626748524c30e8b3819f6f63ac2c0177e1b9
Boyan Sun <bsun68@wisc.edu>
2023-03-26 16:16:11 -0500

    set up restaurant comment model

---
+++ src/main/java/com/tencent/wxcloudrun/model/RestaurantComment.java
@@ -0,0 +1,15 @@
+package com.tencent.wxcloudrun.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class RestaurantComment {
+    private String comment;
+    private String userID;
+    private Integer rating;
+    private Integer price;
+    private ArrayList<String> images;
+    private String imagesJSON;
+}
