8779aaf5e138e44c6df4cc873bbc0e7a718d9455
Boyan Sun <bsun68@wisc.edu>
2023-03-26 16:37:33 -0500

    setup restaurant service and mapper

---
+++ src/main/java/com/tencent/wxcloudrun/dao/RestaurantMapper.java
@@ -0,0 +1,8 @@
+package com.tencent.wxcloudrun.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RestaurantMapper {
+}
---
+++ src/main/java/com/tencent/wxcloudrun/model/Restaurant.java
@@ -0,0 +1,10 @@
+package com.tencent.wxcloudrun.model;
+
+public class Restaurant {
+    private Integer restaurantID;
+    private String name;
+    private String location;
+    private Float avgRating;
+    private Integer avgPrice;
+    private Integer commentCount;
+}
