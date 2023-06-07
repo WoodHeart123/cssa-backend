11e4effab17219d11515e99a7001957ee9cd497a
Boyan Sun <bsun68@wisc.edu>
2023-06-07 08:11:17 +0800

    remove redis inject

--- src/main/java/com/tencent/wxcloudrun/util/CommandLineAppStartupRunner.java
+++ src/main/java/com/tencent/wxcloudrun/util/CommandLineAppStartupRunner.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+//@Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     @Autowired
